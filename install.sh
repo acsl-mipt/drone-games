4e6039c5407e2e6452297f5edfa1468bb03f532b
d3dx13 <36724128+d3dx13@users.noreply.github.com>
2021-03-21 12:48:09 +0300

    Update install.sh
    source ~/catkin_ws/devel/setup.bash

--- install.sh
+++ install.sh
@@ -20,10 +20,13 @@ sudo apt install -y ros-noetic-desktop-full
 source /opt/ros/noetic/setup.bash
 sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 
-sudo apt install -y python3-rosdep
-
 sudo rosdep init
 rosdep update
+
+cd ~/catkin_ws/src; catkin_init_workspace; cd -; cd ~/catkin_ws; catkin_make; cd -;
+
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
 
 if [ "$1" != "build" ]; then
  sudo apt install -y ros-noetic-gazebo-ros-control
