7adebfc2f86f0b86a591978d84a08c0c291b1a0c
d3dx13 <36724128+d3dx13@users.noreply.github.com>
2021-03-21 12:44:05 +0300

    Update install.sh
    sudo rosdep init
    rosdep update

--- install.sh
+++ install.sh
@@ -14,7 +14,16 @@ fi
 ./multiple-sitl/install/$f.sh default $dir/Firmware
 
 # Install full version of ROS Noetic. It could help in development process
+source /opt/ros/noetic/setup.bash
 sudo apt install -y ros-noetic-desktop-full
+
+source /opt/ros/noetic/setup.bash
+sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+
+sudo apt install -y python3-rosdep
+
+sudo rosdep init
+rosdep update
 
 if [ "$1" != "build" ]; then
  sudo apt install -y ros-noetic-gazebo-ros-control
